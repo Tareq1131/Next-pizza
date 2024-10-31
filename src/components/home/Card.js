b4f68dde0eeacaf757fb516aec926be9a8f2670e
Tareq1131 <tareqislam076@gmail.com>
2024-11-01 01:31:20 +0600

    created add to card functionality

--- src/components/home/Card.js
+++ src/components/home/Card.js
@@ -1,10 +1,11 @@
+import { CartContext } from "@/utils/ContextReducer";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useContext, useState } from "react";
 
 function Card(props) {
   const data = props.foodData;
-
+  const { state, dispatch } = useContext(CartContext);
   const priceOptions = Object.keys(data.price);
   const [size, setSize] = useState(priceOptions[0]);
   const [qty, setQty] = useState(1);
@@ -12,29 +13,57 @@ function Card(props) {
   const handleQty = (e) => {
     setQty(e.target.value);
   };
+
   const handleSize = (e) => {
     setSize(e.target.value);
   };
+  const handleAddToCart = async () => {
+    const updateItem = await state.find(
+      (item) => item.tempId === data["_id"] + size
+    );
+    if (!updateItem) {
+      dispatch({
+        type: "ADD",
+        id: data["_id"],
+        tempId: data["_id"] + size,
+        name: data.name,
+        price: finalPrice,
+        qty: qty,
+        priceOption: size,
+        img: data.img,
+      });
+    }
+    if (updateItem) {
+      dispatch({
+        type: "UPDATE",
+        tempId: data["_id"] + size,
+        price: finalPrice,
+        qty: qty,
+      });
+    }
 
+    // console.log(state);
+    //
+  };
   let finalPrice = qty * parseInt(data.price[size]);
+
   return (
     <div className="box">
-      <div className="overflow-hidden bg-white rounded-lg w-80 dark:bg-black border-gradient">
-        {/* <Link href={{ pathname: "/Item/[item]" }} as={`Item/${data["_id"]}`}> */}
-        <div className="relative w-full h-80">
-          <Image src={data.img} layout="fill" objectFit="cover" alt="pizza" />
-          <Image src="" layout="fill" objectFit="cover" alt="pizza" />
-        </div>
-        <div className="p-4">
-          <div lassName="font-bold mb-2 text-xl uppercase ">{data.name}</div>
-          <p className="text-base text-gray-700 short_description dark:text-gray-400">
-            {data.description}
-          </p>
-        </div>
-        {/* </Link> */}
-        <div className="flex justify-between px-4">
+      <div className="w-80 rounded-lg bg-white overflow-hidden dark:bg-black border-gradient">
+        <Link href={{ pathname: "/Item/[item]" }} as={`Item/${data["_id"]}`}>
+          <div className="relative w-full h-80">
+            <Image src={data.img} layout="fill" objectFit="cover" alt="pizza" />
+          </div>
+          <div className="p-4">
+            <div lassName="font-bold mb-2 text-xl uppercase"> {data.name}</div>
+            <p className=" short_description text-gray-700 dark:text-gray-400 text-base">
+              {data.description}
+            </p>
+          </div>
+        </Link>
+        <div className="flex px-4 justify-between">
           <select
-            className="p-1 font-semibold text-black border border-black rounded cursor-pointer h-100 hover:font-bold dark:text-gray-300 dark:border-gray-400"
+            className=" h-100  p-1 text-black hover:font-bold font-semibold cursor-pointer dark:text-gray-300  border border-black dark:border-gray-400 rounded"
             onChange={handleQty}
           >
             {Array.from(Array(6), (e, i) => {
@@ -46,7 +75,7 @@ function Card(props) {
             })}
           </select>
           <select
-            className="p-1 font-semibold text-black border border-black rounded cursor-pointer h-100 hover:font-bold dark:text-gray-300 dark:border-gray-400"
+            className=" h-100  p-1 text-black hover:font-bold font-semibold cursor-pointer dark:text-gray-300  border border-black dark:border-gray-400 rounded"
             onChange={handleSize}
           >
             {priceOptions.map((options) => {
@@ -58,10 +87,10 @@ function Card(props) {
             })}
           </select>
         </div>
-        <div className="flex justify-between p-4 font-bold">
+        <div className="flex p-4 font-bold  justify-between">
           <button
-            // onClick={handleAddToCart}
-            className="p-2 border border-gray-900 rounded dark:border-gray-400 hover:bg-gradient-to-r from-indigo-700 via-violet-700 to-orange-700 hover:text-gray-100 "
+            onClick={handleAddToCart}
+            className="border dark:border-gray-400 border-gray-900 rounded p-2 hover:bg-gradient-to-r from-indigo-700 via-violet-700 to-orange-700  hover:text-gray-100 "
           >
             Add to cart
           </button>
